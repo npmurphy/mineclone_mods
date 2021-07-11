950cf4188dac36e707da494dc37c26694b79aed0
Niall Murphy <nmurphy@gmail.com>
2021-07-11 10:49:50 +0100

    craft diamonds

---
+++ craft_diamonds/init.lua
@@ -0,0 +1,8 @@
+minetest.register_craft({
+	output = 'mcl_core:diamond',
+	recipe = {
+		{"mcl_core:cobble", "mcl_core:cobble", "mcl_core:cobble"},
+		{"mcl_core:cobble", "mcl_core:cobble", "mcl_core:cobble"},
+		{"mcl_core:cobble", "mcl_core:cobble", "mcl_core:cobble"},
+	}
+})
